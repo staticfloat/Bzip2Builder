13145ebb28319d0f063bc06529424c4ff2e915f1
Ian <i.r.butterworth@gmail.com>
2019-05-29 18:46:53 -0400

    add executableproduct for bzip2

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -48,6 +48,7 @@ platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = prefix -> [
+    ExecutableProduct(prefix,"bzip2", :bzip2),
     LibraryProduct(prefix,"libbz2", :libbzip2),
 ]
 
